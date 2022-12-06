ddb05ea6bae4760bdc2e76bd5a2cd5472982a69d
gabbywijaya <gabby.putri@ti.ukdw.ac.id>
2022-12-06 12:39:45 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+      <div class="sidebar">
+          <h2>Daftar Menu</h2>
+          <ul>
+              <li><a href="#"><i class="fas fa-home"></i>Makanan</a></li>
+              <li><a href="#"><i class="fas fa-user"></i>Minuman</a></li>
+              <li><a href="#"><i class="fas fa-address-card"></i>Snack</a></li>
+  
+          </ul> 
+          
+      </div>
+      <div class="main_content">
+          <div class="header">Welcome!! Have a nice day.</div>  
+          <div class="info">
+            <div>Selamat Datang Di Aplikasi Mac Donal, </div>
+            <div>Silahkan Pesan Makanan Anda </div>
+        </div>
+      </div>
+  </div>
+  </template>
+  
+  <script>
+  
+  
+  
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #2c3e50;
+    margin-top: 60px;
+    
+  }
+  </style>
+  
